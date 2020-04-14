62005c9ab4d140381531feb2345079ace7a6f758
Frantz-B <Frantz@kargo.com>
2020-04-14 18:05:18 -0400

    Add logic for sorting stories vs bugs

--- release-notes-helper.js
+++ release-notes-helper.js
@@ -1,12 +1,17 @@
-let pullRequestElements = document.querySelectorAll("a[data-hovercard-type='pull_request'")
-let gitHubString = ""
+let pullRequestRow = document.querySelectorAll('.min-width-0.lh-condensed');
 let kargoLink = "https://kargo1.atlassian.net/browse/"
+let gitHubString = ''
+let slackString = ''
+let storiesGit = [];
+let stories = [];
+let bugGit = [];
+let bug = [];
 
-pullRequestElements.forEach(pullRequest => {
-    let prText = pullRequest.innerText;
-    let dm_id = prText.split('] ')[0].toUpperCase().replace(/[\[\]']+/g, '') // plucking just Jira ticket_id
-    let ticketTitle = prText.split('] ')[1]
-    let pr_id = pullRequest.href.split('pull/')[1]
+pullRequestRow.forEach( pullRequest => {
+    let prText = pullRequest.childNodes[1].innerText;
+    let dm_id = prText.split('] ')[0].toUpperCase().replace(/[\[\]']+/g, ''); // plucking just Jira ticket_id
+    let ticketTitle = prText.split('] ')[1];
+    let pr_id = pullRequest.childNodes[1].href.split('pull/')[1];
 
     gitHubString += `* [[${dm_id}]]`;
     gitHubString += `(${kargoLink + dm_id})`
@@ -14,5 +19,23 @@ pullRequestElements.forEach(pullRequest => {
     gitHubString += ticketTitle
     gitHubString += `    (PR - #${pr_id})`
     gitHubString += '\n'
+
+    slackString += `> • [${dm_id}]`;
+    slackString += ' - '
+    slackString += ticketTitle
+    slackString += '\n'
+
+    if (pullRequest.childNodes[5].innerText.includes('bug')) {
+        bugGit.push(gitHubString);
+        bug.push(slackString);
+        gitHubString = '';
+        slackString = '';
+    } else {
+        storiesGit.push(gitHubString);
+        stories.push(slackString);
+        gitHubString = '';
+        slackString = '';
+    }
 })
 gitHubString
+slackString
